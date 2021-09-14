359a47e8810ba980ad33d14b87fa304cfd7652b7
moonwake769 <exhilaratedreverie288@protonmail.ch>
2021-09-14 22:20:21 +0200

    Update

--- setup.py
+++ setup.py
@@ -1,55 +1,24 @@
-#! /usr/bin/python3
-
-import os
-
-class Setup():
-    """A class to represent setup."""
-    def __init__(self):
-        """Initialize setup attributes."""
-        # Getting the path where the programme will stay.
-        filename_1 = "setup.txt"
-        with open(filename_1, 'r') as path:
-            path = str(path.read()).strip()
-
-        self.path = path
-
-        # Assigning the path in main.py.
-        filename_2 = 'main.py'
-        with open(filename_2, 'r+') as main_py: # r+ does the work of rw
-            lines = main_py.readlines()
-            for i, line in enumerate(lines):
-                if line.startswith('PATH'):
-                    lines[i] = lines[i].strip() + ' ' + f"\"{path}\"\n"
-            main_py.seek(0)
-            for line in lines:
-                main_py.write(line)
-        main_py.close()
-
-        # Assigning the path in fq.
-        filename_3 = 'fq'
-        with open(filename_3, 'r+') as fq: # r+ does the work of rw
-            lines = fq.readlines()
-            for i, line in enumerate(lines):
-                if line.startswith('path'):
-                    lines[i] = lines[i].strip() + f"\"{path}\"\n"
-            fq.seek(0)
-            for line in lines:
-                fq.write(line)
-        fq.close()
-
-    def copy_to_path(self):
-        """Copy the files in order to freely run the fq command."""
-        path = self.path
-        # Copy famous-quotes folder.
-        cmd_1 = f"cp -r $(pwd) {path}"
-        os.system(cmd_1)
-
-        # Copy fq file.
-        cmd_2 = f"cp fq {path}"
-        os.system(cmd_2)
-
-if __name__ == '__main__':
-    Setup().copy_to_path()
-    print("\n\nThe proramme is successfully installed.\n\n")
-    # Display the usage.
-    os.system("fq -h")
+from distutils.core import setup
+setup(
+  name = 'famous-quotes',
+  packages = ['famous-quotes'],
+  version = '0.1',
+  license='MIT',
+  description = "With the assistance of this programme, you can not only display a random quote of a celebrity you admire, but you can also add your own famous quotes. After you added it, is's automatically saved. Then, you can call the programme and you'll have a random quote displayed to you.",   # Give a short description about your library
+  author = 'moonwake769',
+  #author_email = '',
+  url = 'https://github.com/moonwake769/famous-quotes',
+  download_url = 'https://github.com/moonwake769/famous-quotes/archive/refs/tags/v0.1-alpha.tar.gz',
+  keywords = ['quotes', 'python', 'cli', 'famous people'],   # Keywords that define your package best
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
