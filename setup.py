8912e7c31614e06e4759580880fc66a353eb23f9
moonwake769 <exhilaratedreverie288@protonmail.ch>
2021-09-15 01:36:56 +0200

    Update

--- setup.py
+++ setup.py
@@ -2,15 +2,17 @@ from distutils.core import setup
 setup(
   name = 'famous-quotes',
   packages = ['famous-quotes'],
-  version = '0.2.3',
+  version = '0.2.4',
   license='MIT',
   description = "With the assistance of this programme, you can not only display a random quote of a celebrity you admire, but you can also add your own famous quotes. After you added it, is's automatically saved. Then, you can call the programme and you'll have a random quote displayed to you.",   # Give a short description about your library
   author = 'moonwake769',
   #author_email = '',
   url = 'https://github.com/moonwake769/famous-quotes',
-  download_url = 'https://github.com/moonwake769/famous-quotes/archive/refs/tags/v0.2.3-alpha.tar.gz',
+  download_url = 'https://github.com/moonwake769/famous-quotes/archive/refs/tags/v0.2.4-alpha.tar.gz',
   keywords = ['quotes', 'python', 'cli', 'famous people'],   # Keywords that define your package best
+  package_data = {"famous-quotes" : ["*"]},
   install_requires=[],
+  include_package_data = True,
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',
